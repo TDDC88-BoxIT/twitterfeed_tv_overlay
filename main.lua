a7f04958578c440c24278ef5411868453e194f15
Victor Broman <vicbr276@student.liu.se>
2014-11-04 16:42:46 +0100

    The text renderer now works but has not been fully tested The line break function is very primitive

--- main.lua
+++ main.lua
@@ -4,11 +4,11 @@ dir = 'scrum1/static/img/'
 local text_sprite = gfx.loadpng(dir .. 'text_sprite.png')
 x_rel = x_pos
 y_rel = y_pos
-char_width=40
-char_height=40
+char_width=39
+char_height=47
 line_spacing=5
 	for i=1,string.len(text) do
-    screen:copyfrom(text_sprite, {x=(((string.byte(string.sub(text,i,i))-32)%25)*39), y=(math.floor((string.byte(string.sub(text,i,i))-31)/25)*46), w=40, h=47}, {x = x_rel , y = y_rel, w = char_width , h = char_height} ,true)
+    screen:copyfrom(text_sprite, {x=(((string.byte(string.sub(text,i,i))-32)%26)*39), y=(math.floor((string.byte(string.sub(text,i,i))-31)/26)*46), w=40, h=47}, {x = x_rel , y = y_rel, w = char_width , h = char_height} ,true)
     x_rel=x_rel+char_width
     if (x_rel>= max_width) then
       y_rel=(y_rel+char_height)
@@ -18,5 +18,5 @@ line_spacing=5
 end
 
 function onStart()
-  render_text(, 50, 50, 400)
+  render_text("hej!", 50, 50, 400)
 end

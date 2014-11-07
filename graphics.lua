507121c4ba1d1ce2588e09d04ffeb36b66b05489
Sofie Nilsson <sofienilsson90@gmail.com>
2014-11-07 10:49:29 +0100

    Fixed the design of the menu list

--- graphics.lua
+++ graphics.lua
@@ -12,22 +12,41 @@ horizontal_pos = 0
 start = 0
 local menu
 
-require("menu_object")
+require("scrum1.menu_object")
 
 function prompt_channel(channel_list)
-   --Delar upp skärmen i 3 delar på bredden, och 7 delar på höjden.
-  local img1 = gfx.loadpng(dir .. 'boxIT.png')
+  
+  -- Picture of the company logo
+  local company_img = gfx.loadpng(dir .. 'boxIT.png')
+  
+  -- Gets the height and width of the screen
   height = screen:get_height()
   width = screen:get_width()
+  
+  -- Should not use these! 
   width_x = (width-100)/3
   height_y = (height-100)/7
-  --Prints out the large grey box
-  screen:fill(grey1, {x=50,y=50,w=width_x*3, h=height_y*7})
-  screen:copyfrom(img1, nil, {x=50,y=50})
+   
+   -- Prints the tv picture in the background
+   local tv_img = gfx.loadpng(dir .. 'tv_picture.png')
+   screen:copyfrom(tv_img, nil, {x=0,y=0})
+   
+   -- Sets offset for the gray box to 5% of the total width and height
+   local x_offset = width*0.05
+   local y_offset = height*0.05
+   local box_height = height*0.9
+   local box_width = width*0.9
+   
+    --Prints out the large grey box and the company logo
+  screen:fill(grey1, {x=x_offset,y=y_offset,w=box_width, h=box_height})
+  screen:copyfrom(company_img, nil, {x=x_offset,y=y_offset})
   
-  menu = menu_object(width_x,height_y*4)
+  -- Creates a meny object and draws it
+  menu_width = 600
+  menu_height = 400
+  menu = menu_object(menu_width,menu_height)
   add_menu_items()
-  menu:set_background(dir.."tv_picture.png")
+  menu:set_background(dir.."menu_background.png")
   timer = sys.new_timer(100, "update_menu")
   draw_menu()
   
@@ -45,8 +64,9 @@ end
 end
 
 function draw_menu()
-  screen:clear() --Will clear all background stuff
-  screen:copyfrom(menu:get_surface(), nil,{x=100,y=100,width=menu:get_size().width,height=menu:get_size().height},true)
+  -- Do we need this following line of code?!
+  --screen:clear() --Will clear all background stuff
+  screen:copyfrom(menu:get_surface(), nil,{x=(width/2)-(menu_width/2),y=(height/2)-(menu_height/2),width=menu:get_size().width,height=menu:get_size().height},true)
   menu:destroy()
   gfx.update()
 end
@@ -61,9 +81,5 @@ end
 function update_menu()
   draw_menu()
 end
-
-
-
-  
 
 --return prompt

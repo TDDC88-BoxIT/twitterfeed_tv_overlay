6d44b45708b130573e6b051fc2a5ac4edcc85146
Henrik Wellander <henwe610@student.liu.se>
2014-11-12 10:52:51 +0100

    added json files for all channels. to be used as path for download later

---
+++ download_tv_schedule.lua
@@ -0,0 +1,10 @@
+require ("socket.http")
+
+-- adresses:
+-- svt1: http://json.xmltv.se/svt1.svt.se_2014-11-27.js.gz
+-- svt2: http://json.xmltv.se/svt2.svt.se_2014-11-27.js.gz
+-- tv3: http://json.xmltv.se/tv3.se_2014-11-27.js.gz
+-- tv4 : http://json.xmltv.se/tv4.se_2014-11-25.js.gz
+-- kanal 5: http://json.xmltv.se/kanal5.se_2014-11-27.js.gz
+-- tv6 : http://json.xmltv.se/tv6.se_2014-11-27.js.gz
+

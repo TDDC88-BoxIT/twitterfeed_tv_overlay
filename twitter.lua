339b62089abdcdf92281e86c66e73b6486d107e6
Gustav Norén <Gustav@Gustavs-MacBook-Pro.local>
2014-11-12 11:50:43 +0100

    Created timestamps for tweets and functionality to compare two tweet-timestamps

--- twitter.lua
+++ twitter.lua
@@ -32,11 +32,42 @@ function twitter.get_tweets(program_name)
   
   local i = 1
   for k,v in pairs(decoded_tweets.statuses) do
-    tweets[i] = {["name"] = v.user.screen_name, ["text"] = v.text, ["date"] = string.sub(v.created_at, 1, 19) .. string.sub(v.created_at, 26)}
+    local date = string.sub(v.created_at, 1, 19) .. string.sub(v.created_at, 26)
+    --Format timestamp
+    local timestamp = set_timestamp(date);
+
+    tweets[i] = {["name"] = v.user.screen_name, ["text"] = v.text, ["date"] = date, ["timestamp"] = timestamp}
     i = i+1
   end
-  
+
   return tweets
+end
+
+--Formats the twitter date to timestamp format
+function set_timestamp(date)
+  -- body
+  --Convert abbreviated month to number
+  local temp_month = {["Jan"] = 01, ["Feb"] = 02, ["Mar"] = 03, ["Apr"] = 04, ["May"] = 05, ["Jun"] = 06, ["Jul"] = 07, ["Aug"] = 08, ["Sep"] = 09, ["Oct"] = 10, ["Nov"] = 11, ["Dec"] = 12}
+
+  local timestamp = {year = string.sub(date,21,26),
+         month = temp_month[string.sub(date, 5, 7)],
+           day = string.sub(date, 9, 10),
+          hour = string.sub(date, 12, 13),
+           min = string.sub(date, 15, 16), 
+           sec = string.sub(date, 18, 19),
+         isdst = false}
+
+  return timestamp
+end
+
+--Determines if one tweet is newer/older than the other
+function compare_timestamp( t1, t2 )
+  -- body
+  for key,value in pairs(t1) do print(key,value) end
+  for key,value in pairs(t2) do print(key,value) end
+
+  print(os.difftime(os.time(t1),os.time(t2)))
+  print(os.difftime(os.time(t2),os.time(t1)))
 end
 
 

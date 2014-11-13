a8fb07841bc322a1153d1999e8c98300a14e3a05
Gustav Norén <Gustav@Gustavs-MacBook-Pro.local>
2014-11-13 14:58:53 +0100

    Completed functionality to only add tweets newer than the ones already in the tweet list

--- twitter.lua
+++ twitter.lua
@@ -43,10 +43,58 @@ function twitter.get_tweets(program_name)
   return tweets
 end
 
+
+
+function twitter.get_new_tweets(old_tweets)
+  
+  local json = require("json")
+  local decoded = {}
+
+  --Here there will be code that sends the neccessary information to the twitter api so that tweets containing the search term 'program_name' will be returned
+  
+  local new_tweets = {}
+
+-- This part simulates receiving tweets, it reads a json object from a file and decodes it
+  local f = io.open("static/json/paradisehotelse.json","rb")
+  if f then 
+    f:close() 
+  end 
+  if f ~= nil then
+    local lines = ""
+    for line in io.lines("static/json/paradisehotelse.json") do 
+      lines = lines .. line
+    end
+    
+    -- This is where the json object is decoded
+    decoded_tweets = json:decode(lines)
+  end
+  
+  local i = 1
+  for k,v in pairs(decoded_tweets.statuses) do
+    local date = string.sub(v.created_at, 1, 19) .. string.sub(v.created_at, 26)
+    --Format timestamp
+    local timestamp = set_timestamp(date);
+
+    new_tweets[i] = {["name"] = v.user.screen_name, ["text"] = v.text, ["date"] = date, ["timestamp"] = timestamp}
+    i = i+1
+  end
+
+  --Kontrollera här om det finns tweets is new_tweets som är nyare än det sista tweetet i old_tweets
+  for i, v in ipairs(new_tweets) do
+    if(compare_timestamp(old_tweets[table.getn(old_tweets)].timestamp, v.timestamp) > 0) then
+      old_tweets.insert(v)
+    end
+  end
+
+  return old_tweets
+end
+
+
 --Formats the twitter date to timestamp format
 function set_timestamp(date)
   --Convert abbreviated month to number
-  local temp_month = {["Jan"] = 01, ["Feb"] = 02, ["Mar"] = 03, ["Apr"] = 04, ["May"] = 05, ["Jun"] = 06, ["Jul"] = 07, ["Aug"] = 08, ["Sep"] = 09, ["Oct"] = 10, ["Nov"] = 11, ["Dec"] = 12}
+  local temp_month = {["Jan"] = 01, ["Feb"] = 02, ["Mar"] = 03, ["Apr"] = 04, ["May"] = 05, ["Jun"] = 06,
+                     ["Jul"] = 07, ["Aug"] = 08, ["Sep"] = 09, ["Oct"] = 10, ["Nov"] = 11, ["Dec"] = 12}
 
   local timestamp = {year = string.sub(date,21,26),
          month = temp_month[string.sub(date, 5, 7)],
@@ -62,7 +110,7 @@ end
 --Determines if one tweet is newer/older than the other
 function compare_timestamp( t1, t2 )
   --Returns the difference in seconds beween t1 and t2 (could be negative)
-  os.difftime(os.time(t1), os.time(t2))
+  return os.difftime(os.time(t1), os.time(t2))
 end
 
 

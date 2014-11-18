f284fe245b96f82bd15b2af31dfc7ac33983c43a
Sofie Nilsson <sofienilsson90@gmail.com>
2014-11-18 09:16:21 +0100

    Moved channel menu and modified the menu background

--- channel_menu.lua
+++ channel_menu.lua
@@ -1,15 +1,8 @@
 function prompt_channel(channel_list)
 
-  -- Picture of the company logo
-  local company_img = gfx.loadpng(dir .. 'boxIT.png')
-
   -- Gets the height and width of the screen
   height = screen:get_height()
   width = screen:get_width()
-
-  -- Should not use these! 
-  width_x = (width-100)/3
-  height_y = (height-100)/7
 
   -- Prints the tv picture in the background
   draw_tv_screen()
@@ -17,47 +10,42 @@ function prompt_channel(channel_list)
   --screen:copyfrom(tv_img, nil, {x=0,y=0})
 
   -- Sets offset for the gray box to 5% of the total width and height
-  local x_offset = width*0.05
-  local y_offset = height*0.05
+  x_offset = width*0.02
+  y_offset = height*0.05
   local box_height = height*0.9
-  local box_width = width*0.9
+  local box_width = width*0.2
 
   --Prints out the large grey box and the company logo
-  screen:fill(grey1, {x=x_offset,y=y_offset,w=box_width, h=box_height})
-  screen:copyfrom(company_img, nil, {x=x_offset,y=y_offset})
-
+  screen:fill(green1, {x=x_offset,y=y_offset,w=box_width, h=box_height})
+ 
   -- Creates a meny object and draws it
-  menu_width = 600
-  menu_height = 400
-  menu = menu_object(menu_width,menu_height)
+  channel_menu = menu_object(box_width,box_height)
   add_menu_items()
-  menu:set_background(dir.."menu_background.png")
+  channel_menu:set_background(dir.."menu_background.png")
   --timer = sys.new_timer(100, "update_menu") -- removed for now because no need for it in the app
   draw_menu()
-
-  --Get the length of the channel_list dictionary
-  --local list_length = #channel_list
 end
 
   function add_menu_items()
+  --Get the length of the channel_list dictionary
+  --local list_length = #channel_list
     -- menu:add_button("svt1","SVT 1")
     -- menu:add_button("svt1","SVT 2")
     -- menu:add_button("svt1","Kanal 3")
     -- menu:add_button("svt1","TV4")
     -- menu:add_button("svt1","Kanal 5")
     -- menu:add_button("svt1","Kanal 6")
-    menu:add_button("svt1",dir.."tv3.png")
-    menu:add_button("svt1",dir.."tv4.png")
-    menu:add_button("svt1",dir.."kanal5.png")
-    menu:add_button("svt1",dir.."tv6.png")
+    channel_menu:add_button("svt1",dir.."tv3.png")
+    channel_menu:add_button("svt1",dir.."tv4.png")
+    channel_menu:add_button("svt1",dir.."kanal5.png")
+    channel_menu:add_button("svt1",dir.."tv6.png")
 end
 
 function draw_menu()
-
   -- Do we need this following line of code?!
   --screen:clear() --Will clear all background stuff
-  screen:copyfrom(menu:get_surface(), nil,{x=(width/2)-(menu_width/2),y=(height/2)-(menu_height/2),width=menu:get_size().width,height=menu:get_size().  height},true)
-  menu:destroy()
+  screen:copyfrom(channel_menu:get_surface(), nil,{x=x_offset,y=y_offset,width=channel_menu:get_size().width,height=channel_menu:get_size().height},true)
+  channel_menu:destroy()
   gfx.update()
 end
 
@@ -75,7 +63,7 @@ end
 --and moves the red marker if it's suppose to
 function increase_index()
   if am_i_in_menu == 1 then
-    menu:increase_index()
+    channel_menu:increase_index()
     update_menu()
   end
   if am_i_in_menu == 0 then
@@ -87,7 +75,7 @@ end
 --and moves the red marker if it's suppose to
 function decrease_index()
   if am_i_in_menu == 1 then
-    menu:decrease_index()
+    channel_menu:decrease_index()
     update_menu()
   end
   if am_i_in_menu == 0 then

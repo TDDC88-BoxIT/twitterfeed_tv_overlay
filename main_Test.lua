061de0fd21347c865919d2c6f76cf3e91295a02d
Markus Sandin <marsa095@student.liu.se>
2014-11-19 11:27:59 +0100

    Made changes to Main_Test.lua

--- main_Test.lua
+++ main_Test.lua
@@ -6,4 +6,10 @@ package.path = "Test_Twitter/?.lua;" .. package.path
 require("tests.test_tv_info")
 require("tests.test_graphics")
 require("tests.test_twitter")
-os.exit(LuaUnit.run())
+
+lu = LuaUnit.new()
+lu:setOutputType("Junit")
+os.exit( lu:runSuite() )
+
+
+--os.exit(LuaUnit.run())

13fffba1627626144fa164dfb9be2071c7ba43f3
Jesper Asplund <jesper.asplund91@gmail.com>
2014-11-26 12:00:24 +0100

    Added a new smaller sprite to be able to write the date and the top menu text nicer

--- render_text.lua
+++ render_text.lua
@@ -3,6 +3,7 @@ function render_text(text, x_start, y_start, max_width, text_size, text_surface)
 
 dir = 'scrum1/static/img/'
 local text_sprite = gfx.loadpng(dir .. 'text_sprite_courier.png')
+text_sprite_small = gfx.loadpng(dir .. 'text_sprite_courier_small.png')
 a_med_prickar = gfx.loadpng(dir .. 'a_med_prickar.png')
 a_med_cirkel = gfx.loadpng(dir .. 'a_med_cirkel.png')
 o_med_prickar = gfx.loadpng(dir .. 'o_med_prickar.png')
@@ -19,7 +20,11 @@ list_of_words,l = split_word_to_list(text)
     if(line_too_wide(x_pos, list_of_words[i], max_width) and string.len(list_of_words[i])*char_width < max_width) then 
       x_pos,y_pos = break_line(x_start, y_pos)
     end
+    if text_size <= 1.0 then
+    x_pos = write_word(list_of_words[i], x_pos, y_pos, char_width, char_height, text_surface, text_sprite_small)  
+    else
     x_pos = write_word(list_of_words[i], x_pos, y_pos, char_width, char_height, text_surface, text_sprite)
+    end
   end
 
 --Make sure to destroy the sprite in order to conserve RAM
@@ -42,32 +47,60 @@ end
 
 --Writes one single word, gets input where to put it 
 function write_word(word ,x_pos, y_pos, char_width, char_height, text_surface, text_sprite)
-  for i=1,string.len(word) do
-    --The values in this line is based on the width of 
-    --a char in the sprite (39), the height (47), the 
-    --number of columns of char (26). The characters 
-    --in the sprite are arranged in increasing order of
-    --value of the character. The strating value is " " = 32 
-    if string.byte(string.sub(word,i,i),1,1) == 195 then
-    
-    else
-      if string.byte(string.sub(word,i,i),1,1) == 165 then
-      text_surface:copyfrom(a_med_cirkel, {x=12, y=3, w = 23, h = 42}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)
-      elseif string.byte(string.sub(word,i,i),1,1) == 164 then
-      text_surface:copyfrom(a_med_prickar, {x=11, y=8, w = 23, h = 38}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)
-      elseif string.byte(string.sub(word,i,i),1,1) == 182 then
-      text_surface:copyfrom(o_med_prickar, {x=10, y=8, w = 24, h = 37}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)
-    else
-    text_surface:copyfrom(text_sprite, {x=((string.byte(string.sub(word,i,i))-32)*49), y=12, w = 49, h = 67}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)--(math.floor((string.byte(string.sub(word,i,i))-32)/94)*25), w=40, h=47}, {x = x_pos , y = y_pos, w = char_width , h = char_height} ,true)
+  -- At the moment the code is doubled just because we load 2 different sprites and need to read from them in different ways.
+  if text_sprite == text_sprite_small then
+    for i=1,string.len(word) do
+      --The values in this line is based on the width of 
+      --a char in the sprite (39), the height (47), the 
+      --number of columns of char (26). The characters 
+      --in the sprite are arranged in increasing order of
+      --value of the character. The strating value is " " = 32 
+      if string.byte(string.sub(word,i,i),1,1) == 195 then
+      
+      else
+        if string.byte(string.sub(word,i,i),1,1) == 165 then
+        text_surface:copyfrom(a_med_cirkel, {x=12, y=3, w = 23, h = 42}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)
+        elseif string.byte(string.sub(word,i,i),1,1) == 164 then
+        text_surface:copyfrom(a_med_prickar, {x=11, y=8, w = 23, h = 38}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)
+        elseif string.byte(string.sub(word,i,i),1,1) == 182 then
+        text_surface:copyfrom(o_med_prickar, {x=10, y=8, w = 24, h = 37}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)
+      else
+      text_surface:copyfrom(text_sprite, {x=((string.byte(string.sub(word,i,i))-32)*25), y=9, w = 25, h = 37}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)--(math.floor((string.byte(string.sub(word,i,i))-32)/94)*25), w=40, h=47}, {x = x_pos , y = y_pos, w = char_width , h = char_height} ,true)
+      end
+      --x_pos=x_pos+(4*char_width/5)
+      x_pos=x_pos+char_width
+      end
     end
-    --x_pos=x_pos+(4*char_width/5)
+    x_pos=x_pos+char_width
+  
+  else
+    for i=1,string.len(word) do
+      --The values in this line is based on the width of 
+      --a char in the sprite (39), the height (47), the 
+      --number of columns of char (26). The characters 
+      --in the sprite are arranged in increasing order of
+      --value of the character. The strating value is " " = 32 
+      if string.byte(string.sub(word,i,i),1,1) == 195 then
+      
+      else
+        if string.byte(string.sub(word,i,i),1,1) == 165 then
+        text_surface:copyfrom(a_med_cirkel, {x=12, y=3, w = 23, h = 42}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)
+        elseif string.byte(string.sub(word,i,i),1,1) == 164 then
+        text_surface:copyfrom(a_med_prickar, {x=11, y=8, w = 23, h = 38}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)
+        elseif string.byte(string.sub(word,i,i),1,1) == 182 then
+        text_surface:copyfrom(o_med_prickar, {x=10, y=8, w = 24, h = 37}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)
+      else
+      text_surface:copyfrom(text_sprite, {x=((string.byte(string.sub(word,i,i))-32)*49), y=12, w = 49, h = 67}, { x= x_pos, y = y_pos, w = char_width, h = char_height}, true)--(math.floor((string.byte(string.sub(word,i,i))-32)/94)*25), w=40, h=47}, {x = x_pos , y = y_pos, w = char_width , h = char_height} ,true)
+      end
+      --x_pos=x_pos+(4*char_width/5)
+      x_pos=x_pos+char_width
+      end
+    end
     x_pos=x_pos+char_width
     end
-  end
-  x_pos=x_pos+char_width
-
-  return x_pos
+    return x_pos
 end
+--@author Victor, Jesper
 
 --Splits the incoming string to list in order to write the text word-by-word
 --This makes line break testing much easier.

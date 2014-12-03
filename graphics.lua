c07e15bb0ee1cc9244970644a6356ba122290fc6
Henrik Wellander <henwe610@student.liu.se>
2014-12-03 10:09:29 +0100

    changed infotext in tweet view, changed timer

--- graphics.lua
+++ graphics.lua
@@ -125,13 +125,13 @@ function draw_tweet(tweets)
     info_box = gfx.new_surface(800,130)
     info_box:fill(grey4)
     --info_box:copyfrom(info_box_image, nil,nil,true)
-    render_text("To see next or previous tweet press DOWN or UP. If you wish to view the tweets in a different way use RIGHT and LEFT. When you want to watch another channel press BACK.", 5, 5, 800, 1.5, info_box)
+    render_text("UP/DOWN:Next tweet, RIGHT/LEFT:Viewmode, BACK:Menu, MENU:Exit app", 5, 5, 800, 1.5, info_box)
     screen:copyfrom(info_box,nil,{x = screen:get_width()/2-400, y = screen:get_height()-240},{x=100,y=100, w=400, h =200},true)
     info_box:destroy()
     --info_box_image:destroy()
     -- timer currently set to 12 seconds.
     if help_timer == nil then
-      help_timer = sys.new_timer(15000, "clear_info_box")
+      help_timer = sys.new_timer(27000, "clear_info_box")
     end
   end
   
